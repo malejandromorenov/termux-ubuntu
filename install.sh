0a94e4c5a2e2769767263cf5965f9f1583902e1b
apolbox <ayus.sahabat@gmail.com>
2019-05-08 08:22:15 +0700

    Create file install.sh

---
+++ install.sh
@@ -0,0 +1,22 @@
+#
+# Cloning sources termux-ubuntu
+#
+JAILS=~/jails/ubuntu
+
+if [ -d $JAILS ]
+then
+	echo "Direktori $JAILS sudah ada.";
+else
+	echo "Membuat direktori $JAILS";
+	mkdir -p $JAILS
+fi
+
+# Masuk ke direktori jails/ubuntu
+cd $JAILS
+
+# Mengunduh script shell untuk install ubuntu
+wget https://raw.githubusercontent.com/Neo-Oli/termux-ubuntu/master/ubuntu.sh
+
+# Mengubah dan menjalankan
+chmod +x ./ubuntu.sh
+./ubuntu.sh
